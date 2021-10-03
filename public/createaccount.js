e00e863cd1d8c1c2f6e52df96a829662ba0ed1a9
greg <gman.international@gmail.com>
2021-10-03 14:25:08 -0400

    2021_10_03_v1

--- public/createaccount.js
+++ public/createaccount.js
@@ -1,79 +1,71 @@
 function CreateAccount(){
-    const [show, setShow]     = React.useState(true);
-    const [status, setStatus] = React.useState('');
-  
-    return (
-      <Card
-        bgcolor="primary"
-        header="Create Account"
-        status={status}
-        body={show ? 
-          <CreateForm setShow={setShow}/> : 
-          <CreateMsg setShow={setShow}/>}
-      />
-    )
-  }
+  const [show, setShow]     = React.useState(true);
+  const [status, setStatus] = React.useState('');    
 
-  function CreateMsg(props){
-    return(<>
-      <h5>Success</h5>
-      <button type="submit" 
-        className="btn btn-light" 
-        onClick={() => props.setShow(true)}>Add another account</button>
-    </>);
-  }
+  return (
+    <Card
+      bgcolor="primary"
+      header="Create Account"
+      //status={status}
+      body={show ? 
+        <CreateForm setShow={setShow} setStatus={setStatus}/> :
+        <CreateMsg setShow={setShow} setStatus={setStatus}/>}
+    />
+  ) 
+}
 
-  
+function CreateMsg(props){
+  return(<>
+    <h5>Success</h5>
+    <button type="submit" 
+      className="btn btn-light" 
+      onClick={() => props.setShow(true)}>
+        Create a New Account
+    </button>
+  </>);
+}
+
 function CreateForm(props){
-    const [name, setName]         = React.useState('');
-    const [email, setEmail]       = React.useState('');
-    const [password, setPassword] = React.useState('');
-   // const ctx = React.useContext(UserContext);  
-  
-    // function handle(){
-    //   console.log(name,email,password);
-    //   ctx.users.push({name,email,password});
-    //   props.setShow(false);
-    // }    
-  
-    function handle(){
-      console.log(name,email,password);
-      const url = `/account/create/${name}/${email}/${password}`;
-      (async () => {
-        var res = await fetch(url);
-        var data = await res.json();
-        console.log(data);
-      })();
-      props.setShow(false);
-    }
+  const [email, setEmail]     = React.useState('');
+  const [password, setPassword]     = React.useState('');
+  const [err, setErr]     = React.useState('');
 
+  const firebaseConfig = {
+    apiKey: "AIzaSyAzgqUWMGJpv_KWfWhhlEbNsCi-eaC3PRk",
+    authDomain: "gregory-shaw-banking-app.firebaseapp.com",
+    projectId: "gregory-shaw-banking-app",
+    storageBucket: "gregory-shaw-banking-app.appspot.com",
+    messagingSenderId: "637460095620",
+    appId: "1:637460095620:web:a5c6e0a44d952468d218d1"
+  };
 
-    return (<>
-  
-      Name<br/>
-      <input type="input" 
-        className="form-control" 
-        placeholder="Enter name" 
-        value={name} 
-        onChange={e => setName(e.currentTarget.value)} /><br/>
-  
-      Email address<br/>
-      <input type="input" 
-        className="form-control" 
-        placeholder="Enter email" 
-        value={email} 
-        onChange={e => setEmail(e.currentTarget.value)}/><br/>
-  
-      Password<br/>
-      <input type="password" 
-        className="form-control" 
-        placeholder="Enter password" 
-        value={password} 
-        onChange={e => setPassword(e.currentTarget.value)}/><br/>
-  
-      <button type="submit" 
-        className="btn btn-light" 
-        onClick={handle}>Create Account</button>
-  
-    </>);
-  }
+!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
+
+  const handleCreate = (e) => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    var user = userCredential.user;
+    console.log("user" + user)
+    props.setShow(false);
+  })
+  .catch((e) => {
+    console.log(e.message)
+    setErr(e.message)
+    setEmail('')
+    setPassword('')
+  });
+}
+
+return (
+<>
+   Email address<br/>
+   <input className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email"/><br></br>
+   Password<br/>
+   <input className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password"/><br></br>
+   <button className="btn btn-light" onClick={handleCreate}>Create Account</button>
+   <br></br>
+   <br></br>
+   <p>{err ? 'ERROR: ' + err : ''}</p>
+</>   
+)
+}
--- public/login.js
+++ public/login.js
@@ -1,101 +1,148 @@
-
-
 function Login(){
-  //const [show, setShow]     = React.useState(true);
-  //const [status, setStatus] = React.useState('');  
-
-  const handleChange = event => {
-    console.log(`handleChange ${event.target.value}`);
-    const email = event.target.value;
-    console.log(email)
-  };
+  const [show, setShow]     = React.useState(true);
+  const [status, setStatus] = React.useState('');    
 
   return (
-    //<h1>Test</h1>
     <Card
       bgcolor="secondary"
       header="Login"
       //status={status}
-      body={
+      body={show ? 
+        <LoginForm setShow={setShow} setStatus={setStatus}/> :
+        <LoginMsg setShow={setShow} setStatus={setStatus}/>}
+    />
+  ) 
+}
+
+function LoginMsg(props){
+  return(<>
+    <h5>Success</h5>
+    <button type="submit" 
+      className="btn btn-light" 
+      onClick={() => props.setShow(true)}>
+        Authenticate again
+    </button>
+    {/* <button type="submit" 
+      className="btn btn-light" onClick={handleLogout}>Logout</button> */}
+  </>);
+}
+
+
+function LoginForm(props){
+  const [email, setEmail]     = React.useState('');
+  const [password, setPassword]     = React.useState('');
+  const [err, setErr]     = React.useState('');
+  //const [uid, setUid]     = React.useState();
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAzgqUWMGJpv_KWfWhhlEbNsCi-eaC3PRk",
+    authDomain: "gregory-shaw-banking-app.firebaseapp.com",
+    projectId: "gregory-shaw-banking-app",
+    storageBucket: "gregory-shaw-banking-app.appspot.com",
+    messagingSenderId: "637460095620",
+    appId: "1:637460095620:web:a5c6e0a44d952468d218d1"
+  };
+
+!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
+//firebase.initializeApp(firebaseConfig);
+  const handleSignUp = (e) => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    var user = userCredential.user;
+    console.log("user" + user)
+    props.setShow(false);
+  })
+  .catch((error) => {
+    console.log(e.message)
+    setErr(e.message)
+    setEmail('')
+    setPassword('')
+  });
+}
+
+    const handleLogin = (e) => {
+      firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+      var user = userCredential.user;
+        console.log("user" + user)
+        props.setShow(false);
+        })
+        .catch((e) => {
+          console.log(e.message)
+          setErr(e.message)
+          setEmail('')
+          setPassword('')
+        });
+      }
+
+  //Google Login
+  const handleGoogle = (e) => {
+    console.log("google clicked");
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        console.log(`You are logged in using the following email: ${user.email}`);
+        props.setShow(false);
+      })
+      .catch((e) => {
+        console.log(e.message)
+        setErr(e.message)
+        setEmail('')
+        setPassword('')
+      });
+  };
+
+  const handleLogout = (e) => {
+    firebase.auth().signOut().then(() => {
+      props.setShow(true)
+    }).catch((e) => {
+      console.log(e.message)
+      setErr(e.message)
+    });
+  }
+    
+    
+  //   firebase.auth().signOut();
+  //   props.setShow(true)
+  // };
+
+    // firebase.auth().onAuthStateChanged((firebaseUser) => {
+    // if (firebaseUser) {
+    //   console.log(firebaseUser);
+    // } else {
+    //   console.log("User is not logged in");
+    // }})
+
+
+console.log(email, password)
+
+return (
+    // <Card
+    //   bgcolor="secondary"
+    //   header="Login"
+    //   body={
 <>
    <h1 id="loggedInStatus">You are not yet logged in</h1>
-   <input onChange={handleChange} id="email" type="email" placeholder="Email"/><br></br>
-   <input id="password" type="password" placeholder="Password"/><br></br>
-   <button id="login">Login</button>
-   <button id="signup">SignUp</button>
-   <button id="logout">Logout</button> 
-   {/* style="display: none;" */}
-   {/* <Goog/> */}
-  {/*
-  <hr>
-  <button id="googlelogin">Google Login</button>
- */}
+   Email address<br/>
+   <input className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email"/><br></br>
+   Password<br/>
+   <input className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password"/><br></br>
+   <button className="btn btn-light" onClick={handleLogin}>Login</button><br/>
+   <button className="btn btn-light" onClick={handleSignUp}>SignUp</button>
+   <button className="btn btn-light" onClick={handleLogout}>Logout</button>
+   <button className="btn btn-light" onClick={handleGoogle}>Google Login</button>
+   <br></br>
+   <br></br>
+   <p>{err ? 'ERROR: ' + err : ''}</p>
 </>
-}
-    />
-
-    ) 
-
-
-
-  //(
-    function Goog(){
-    
-
-    // Import the functions you need from the SDKs you need
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-    
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-      apiKey: "AIzaSyAzgqUWMGJpv_KWfWhhlEbNsCi-eaC3PRk",
-      authDomain: "gregory-shaw-banking-app.firebaseapp.com",
-      projectId: "gregory-shaw-banking-app",
-      storageBucket: "gregory-shaw-banking-app.appspot.com",
-      messagingSenderId: "637460095620",
-      appId: "1:637460095620:web:a5c6e0a44d952468d218d1"
-    };
-   //import { initializeApp } from "firebase/app";
-  // Initialize Firebase
-  !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
-  //firebase.initializeApp(firebaseConfig);
-
-  // get elements
-  const email = this.getId("email")
-  const password = this.password;
-  const login = this.login;
-  const signup = this.signup;
-  const logout = this.logout;
-
-  // const email = document.getElementById("email");
-  // const password = document.getElementById("password");
-  // const login = document.getElementById("login");
-  // const signup = document.getElementById("signup");
-  // const logout = document.getElementById("logout");
-  //const loggedInStatus = document.getElementById("loggedInStatus");
-  //const googlelogin = document.getElementById("googlelogin");
-
-  // login
-  login.addEventListener("click", (e) => {
-    const auth = firebase.auth();
-    const promise = auth.signInWithEmailAndPassword(
-      email.value,
-      password.value
-    );
-    promise.catch((e) => console.log(e.message));
-  });
-
-  // signup
-  signup.addEventListener("click", (e) => {
-    // TODO: check for real email
-    const auth = firebase.auth();
-    const promise = auth.createUserWithEmailAndPassword(
-      email.value,
-      password.value
-    );
-    promise.catch((e) => console.log(e.message));
-  });
-
+//}
+// />
+)}
   //Google Login
   // googlelogin.addEventListener("click", (e) => {
   //   console.log("google clicked");
@@ -118,10 +165,9 @@ function Login(){
   //     });
   // });
 
-  // logout
-  logout.addEventListener("click", (e) => {
-    firebase.auth().signOut();
-  });
+
+
+
 
   // login state
   // firebase.auth().onAuthStateChanged((firebaseUser) => {
@@ -145,79 +191,31 @@ function Login(){
   //     logout.style.display = "none";
   //   }
   // });
-}
-//)();
 
+    
+  //   const auth = firebase.auth();
+  //   const promise = auth.createUserWithEmailAndPassword(
+  //     email,
+  //     password
+  //   );
+  //   promise.catch((e) => {
+  //     console.log(e.message)
+  //     setErr(e.message)
+  //     setEmail('')
+  //     setPassword('')
+  //   })
+  // }
+  // const handleLogin = (e) => {
+  //   const auth = firebase.auth();
+  //   const promise = auth.signInWithEmailAndPassword(
+  //     email, password
+  //   );
 
+  //   promise.catch((e) => {
+  //     console.log(e.message)
+  //     setErr(e.message)
+  //     setEmail('')
+  //     setPassword('')
+  //   });
+  //   }
 
-}
-
-// OLD LOGIN
-// function Login(){
-//     const [show, setShow]     = React.useState(true);
-//     const [status, setStatus] = React.useState('');    
-  
-//     return (
-//       <Card
-//         bgcolor="secondary"
-//         header="Login"
-//         status={status}
-//         body={show ? 
-//           <LoginForm setShow={setShow} setStatus={setStatus}/> :
-//           <LoginMsg setShow={setShow} setStatus={setStatus}/>}
-//       />
-//     ) 
-//   }
-  
-//   function LoginMsg(props){
-//     return(<>
-//       <h5>Success</h5>
-//       <button type="submit" 
-//         className="btn btn-light" 
-//         onClick={() => props.setShow(true)}>
-//           Authenticate again
-//       </button>
-//     </>);
-//   }
-  
-//   function LoginForm(props){
-//     const [email, setEmail]       = React.useState('');
-//     const [password, setPassword] = React.useState('');
-  
-//     function handle(){
-//       fetch(`/account/login/${email}/${password}`)
-//       .then(response => response.text())
-//       .then(text => {
-//           try {
-//               const data = JSON.parse(text);
-//               props.setStatus('');
-//               props.setShow(false);
-//               console.log('JSON:', data);
-//           } catch(err) {
-//               props.setStatus(text)
-//               console.log('err:', text);
-//           }
-//       });
-//     }
-  
-  
-//     return (<>
-  
-//       Email<br/>
-//       <input type="input" 
-//         className="form-control" 
-//         placeholder="Enter email" 
-//         value={email} 
-//         onChange={e => setEmail(e.currentTarget.value)}/><br/>
-  
-//       Password<br/>
-//       <input type="password" 
-//         className="form-control" 
-//         placeholder="Enter password" 
-//         value={password} 
-//         onChange={e => setPassword(e.currentTarget.value)}/><br/>
-  
-//       <button type="submit" className="btn btn-light" onClick={handle}>Login</button>
-     
-//     </>);
-//   }
